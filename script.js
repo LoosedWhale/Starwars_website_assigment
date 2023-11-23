59ca969e10bbae9c0c054abf9b3cb20fbe963fec
LoosedWhale <81869656+LoosedWhale@users.noreply.github.com>
2023-11-23 16:49:00 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+/*Fancy StarWars thingy | https://www.youtube.com/watch?v=W5oawMJaXbU*/
+
+function animateHeader() {
+  const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let interval = null;
+  const header = document.querySelector("h4");
+
+  header.onmouseover = event => {
+    let iteration = 0;
+    clearInterval(interval);
+
+    interval = setInterval(() => {
+      event.target.innerText = event.target.innerText
+        .split("")
+        .map((letter, index) => {
+          if (index < iteration) {
+            return event.target.dataset.value[index];
+          }
+
+          return letters[Math.floor(Math.random() * 26)];
+        })
+        .join("");
+
+      if (iteration >= event.target.dataset.value.length) {
+        clearInterval(interval);
+      }
+
+      iteration += 1 / 3;
+    }, 30);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", animateHeader);
+
